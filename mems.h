37e66bdfc5af406399c685ba722efc9d09638bc2
Mayankk004 <149898546+Mayankk004@users.noreply.github.com>
2023-11-07 22:10:23 +0530

    Update mems.h

--- mems.h
+++ mems.h
@@ -25,6 +25,8 @@ void* next_mems_virtual_address = (void*)1000;
 void* next_mems_physical_address = (void*)1000;
 
 int t=0;
+int v=0;
+int q=0;
 
 Node* head = NULL;
 void* mem_start = NULL;
@@ -69,8 +71,26 @@ return (void*)((char*)c->virtual_address);
 c = c->next;
 }
 
-size_t num_pages = (size + PAGE_SIZE - 1) / PAGE_SIZE;
-size_t total_size = num_pages * PAGE_SIZE;
+if(4096-v<size){
+void* allocated_memory = mmap(NULL, (size_t)4096-v, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+Node* new = (Node*)allocated_memory;
+new->virtual_address = next_mems_virtual_address;
+new->physical_address = (void*)((char*)new + (size_t)4096-v);
+next_mems_virtual_address = (void*)((char*)next_mems_virtual_address + (size_t)4096-v);
+next_mems_physical_address = (void*)((char*)next_mems_virtual_address + (size_t)4096-v);
+new->size = (size_t)4096-v;
+new->is_hole=1;
+v=0;
+if (head) {
+new->next = head;
+new->prev = NULL;
+head->prev = new;
+head=new;
+}
+}
+
+size_t total_size = size;
+v=v+size;
 void* allocated_memory = mmap(NULL, total_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
 if (allocated_memory == MAP_FAILED) {
 printf(" mmap  Error");
@@ -116,7 +136,6 @@ return;
 }
 c = c->next;
 }
-
 fprintf(stderr, "Error -> Invalid pointer or already freed\n");
 }
 
@@ -132,6 +151,9 @@ Node* subchain_end = c;
 
 while (c && c->page == subchain_start->page) {
 subchain_end = c;
+if(c->is_hole){
+break;
+}
 c = c->prev;
 }   
        
@@ -139,13 +161,21 @@ printf("MAIN[%lu:%lu]-> ", (unsigned long)subchain_start->virtual_address, (unsi
 c = subchain_start;
 while (c && c->page == subchain_start->page) {
 if (c->is_hole) {
+if(c->next->is_hole){
+c=c->prev;
+continue;
+}
+if(c->prev->is_hole) {
+c->size+=c->prev->size;
+}
 printf("H");
 printf("[%lu:%lu] <-> ", (unsigned long)c->virtual_address, (unsigned long)c->virtual_address + c->size - 1);
 t++;
 k[w]=t;
 w++;
 t=0;
-printf("NULL\n                   ");
+c = c->prev;
+break;
 } 
 else {
 printf("P");
@@ -160,25 +190,28 @@ printf("NULL\n");
 
 size_t total_pages = 0;
 size_t unused_memory = 0;
-
+size_t memory = 0;
 c = head;
 while (c) {
-total_pages += c->size / PAGE_SIZE;
+memory += c->size;
 if (c->is_hole) {
 unused_memory += c->size;
 }
 c = c->next;
 }
 
-printf("\nPages used: %lu\n", total_pages);
+printf("\nPages used: %lu\n", memory/4096+1);
 printf("Space unused: %lu\n", unused_memory);
-printf("Main Chain Length: %lu\n", total_pages);
+printf("Main Chain Length: %lu\n", memory/4096+1);
 
     
 printf("Sub-chain Length array: [");
 for (int i = 0; i < MAX_PAGES; i++) {
 if ( k[i]==0) {
 break;
+}
+if ( k[i+1]==0) {
+k[i]++;
 }
 printf("%d, ", k[i]);
 }
